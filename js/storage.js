fe279dfd6a94f2e97c4d55491844a6b00dd47d65
jhonatan padilla <jhoalparo1991@gmail.com>
2022-02-02 17:37:59 -0500

    Se modifica worlds->WORDS

--- js/storage.js
+++ js/storage.js
@@ -1,11 +1,14 @@
 // import { world} from './worlds.js';
 
+import { WORDS } from "./words.js";
+
 export function save(arr){
     let data = JSON.stringify(arr);
     window.localStorage.setItem('palabras',data);
 }
 
 export function show_data(){
-   let data = JSON.parse(localStorage.getItem('palabras'));
+   let data = JSON.parse(localStorage.getItem('palabras')) || WORDS ;
+
    return data;
 }
